4e672f8f631fb28784c14c78dd5e761c413636d6
Aniket Thapa <aniketthapa04@gmail.com>
2024-12-06 00:08:07 +0530

    Installed Tailwind CSS via npm

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.ejs',
+    './public/html/**/*.html',
+    './public/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
