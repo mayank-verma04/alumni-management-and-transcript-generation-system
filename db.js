4fe4657028d60eb06f9b22da9641c6e61167d625
Aniket Thapa <aniketthapa04@gmail.com>
2025-06-23 21:39:57 +0530

    updated mysql connection for online database

--- db.js
+++ db.js
@@ -1,19 +1,29 @@
 const mysql = require('mysql2');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-});
+};
+
+if (process.env.NODE_ENV == 'production') {
+  dbConfig.ssl = {
+    rejectUnauthorized: true,
+    ca: process.env.DB_SSL_CA,
+  };
+}
+
+const db = mysql.createConnection(dbConfig);
 
 db.connect = (err) => {
-  if (err) throw err;
-  console.log('Connected to Local MySQL database');
+  if (err) {
+    console.error('Database connection error:', err);
+    throw err;
+  }
+  console.log('Connected to MySQL database');
 };
 
 module.exports = db;
